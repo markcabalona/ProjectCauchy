92f640e18bed822b97811b483f7cee675e422bea
markcabalona <schmosby@LAPTOP-HNOVMVCO.localdomain>
2024-07-22 21:26:37 +0800

    feature: add new GET api-endpoint `/roulette`

--- game-server/server/app.py
+++ game-server/server/app.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
-from games.poker import simulate_poker
-from games.bigwheel import simulate_bigwheel
 from dataclasses import asdict
+
+from fastapi import FastAPI
+from games.bigwheel import simulate_bigwheel
+from games.poker import simulate_poker
+from games.roulette import simulate_roulette
 
 app = FastAPI()
 
@@ -12,3 +14,8 @@ def get_poker():
 @app.get("/bigwheel")
 def get_bigwheel():
     return asdict(simulate_bigwheel())
+
+
+@app.get("/roulette")
+def get_roulette():
+    return asdict(simulate_roulette())
