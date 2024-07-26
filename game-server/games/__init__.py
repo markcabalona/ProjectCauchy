e8c2c3420be58ddaa59526a1c7bfbc969dce2b79
markcabalona <schmosby@LAPTOP-HNOVMVCO.localdomain>
2024-07-27 06:57:36 +0800

    feature: implement `simulate_roulette` Roulette simulation

--- game-server/games/__init__.py
+++ game-server/games/__init__.py
@@ -1,3 +1,4 @@
 from .poker import simulate_poker
 from .bigwheel import simulate_bigwheel
 from .baccarat import simulate_baccarat
+from .roulette import simulate_roulette
--- game-server/games/roulette/sim.py
+++ game-server/games/roulette/sim.py
@@ -1,11 +1,41 @@
-
 from dataclasses import dataclass
+
+from typing import Dict, List
+from copy import deepcopy
+
+from games.roulette.bet import RouletteBet
+from games.roulette.random_bet import random_bet
+
+
+from .roulette_game import RouletteGame, RoulettePocket
 
 
 @dataclass
 class RouletteResult:
-    pass
+    winning_pocket: RoulettePocket
+    winning_bets: Dict
+    bets: List
 
 
 def simulate_roulette() -> RouletteResult:
-    return RouletteResult(); 
+    winning_pocket = RouletteGame().spin()
+
+    bets = [random_bet() for _ in range(100)]
+
+    winning_bets = list(
+        map(
+            _compute_winnings,
+            [bet for bet in deepcopy(bets) if winning_pocket in bet.pockets],
+        )
+    )
+
+    return RouletteResult(
+        winning_pocket=winning_pocket,
+        winning_bets=winning_bets,
+        bets=bets,
+    )
+
+
+def _compute_winnings(bet: RouletteBet) -> RouletteBet:
+    bet.compute_winnings()
+    return bet
