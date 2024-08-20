86c1e7ddddcb89f79569607639f2fe88077d89bf
markcabalona <markcabalona58@gmail.com>
2024-08-20 12:50:16 +0800

    refactor-roulette: remove data models for roulette game

--- app-server/games/roulette/extract.py
+++ app-server/games/roulette/extract.py
@@ -1,16 +1,15 @@
 import json
 from typing import List
 from requests import get, Response
-from .models import RouletteResponse
 
 
-def extract(base_url: str, games_count: int = 1) -> List[RouletteResponse]:
+def extract(base_url: str, games_count: int = 1) -> json:
     url = f"http://{base_url}:8000/roulette"
 
-    result: List[RouletteResponse] = []
+    result: List = []
     for _ in range(games_count):
         response: Response = get(url)
         if response.ok:
-            result.append(RouletteResponse.from_json(json.dumps(response.json())))
+            result.append(response.json())
 
     return result
