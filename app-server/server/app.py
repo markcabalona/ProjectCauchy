c0731bf945c26ebad1d5ec3d1511fe913d09f179
markcabalona <markcabalona58@gmail.com>
2024-08-20 13:28:17 +0800

    chore: format server/app.py

--- app-server/server/app.py
+++ app-server/server/app.py
@@ -14,13 +14,15 @@ from games.baccarat import pipeline_baccarat
 from games.roulette import roulette_pipeline
 from games.blackjack import blackjack_pipeline
 
+
 def create_directory_if_not_exists(directory_path):
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
         print(f"Directory '{directory_path}' created.")
 
 
-create_directory_if_not_exists('data')
+create_directory_if_not_exists("data")
+
 roulette_pipeline(base_url=URL)
 blackjack_pipeline(base_url=URL)
 pipeline_bigwheel(URL)
